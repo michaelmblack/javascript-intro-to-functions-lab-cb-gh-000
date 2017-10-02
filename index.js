7441fc003274aa1142267fd18221c6335b714f0e
Michael Black <mblack@iastate.edu>
2017-10-02 00:01:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   else if(string === shout(string)){
     return "YES INDEED!";
   }
-  else if(string === "I love you, too."){
-    return "I love you, Grandma"
+  else if(string === "I love you, Grandma."){
+    return "I love you, too"
   }
 }
